052726de0f3be1c35e432652f670c716f8b773b6
Emmanuele Boerci <eboerci91@gmail.com>
2018-03-19 23:30:53 +0100

    Add files via upload

---
+++ 16_prompts.py
@@ -0,0 +1,16 @@
+
+# print('How old  are you?')
+# age = input();
+# print('You are %s' %age)
+
+
+# python 16_prompts.py pippo pluto paperino
+
+from sys import argv
+
+script, first, second, third = argv # unpacking variables
+
+print ("The script is called:", script)
+print ("Your first variable is:", first)
+print ("Your second variable is:", second)
+print ("Your third variable is:", third)
